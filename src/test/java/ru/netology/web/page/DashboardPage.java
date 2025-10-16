45dfe73aabc0921b48bbf7b3329efda0c4c74af4
Anastasia <nastya.il03@mail.ru>
2025-10-16 16:19:17 +0400

    Initial commit

---
+++ src/test/java/ru/netology/web/page/DashboardPage.java
@@ -0,0 +1,23 @@
+package ru.netology.web.page;
+
+public class DashboardPage {
+    // к сожалению, разработчики не дали нам удобного селектора, поэтому так
+    private ElementsCollection cards = $$(".list__item div");
+    private final String balanceStart = "баланс: ";
+    private final String balanceFinish = " р.";
+
+    public DashboardPage() {
+    }
+
+    public int getFirstCardBalance() {
+        var text = cards.first().text();
+        return extractBalance(text);
+    }
+
+    private int extractBalance(String text) {
+        var start = text.indexOf(balanceStart);
+        var finish = text.indexOf(balanceFinish);
+        var value = text.substring(start + balanceStart.length(), finish);
+        return Integer.parseInt(value);
+    }
+}
