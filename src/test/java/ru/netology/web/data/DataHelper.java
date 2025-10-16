29017e7ffbc96fc55bfaf6844d4341badff148fd
Anastasia <nastya.il03@mail.ru>
2025-10-16 17:01:23 +0400

    Add DataHelper

---
+++ src/test/java/ru/netology/web/data/DataHelper.java
@@ -0,0 +1,30 @@
+package ru.netology.web.data;
+
+
+import lombok.Value;
+
+public class DataHelper {
+
+    private DataHelper() {
+    }
+
+    public static AuthInfo getAuthInfo() {
+        return new AuthInfo("vasya", "qwerty123");
+    }
+
+    public static VerificationCode getVerificationCode(AuthInfo authInfo) {
+        return new VerificationCode("12345");
+    }
+
+    @Value
+    public static class AuthInfo {
+        String login;
+        String password;
+    }
+
+    @Value
+    public static class VerificationCode {
+        String code;
+    }
+
+}
